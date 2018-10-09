44902166457e3fbcba2c44ae0cabc73907493758
Roy Peled <peled.roy@gmail.com>
2018-10-09 18:25:41 +0300

    added remote db

--- server/db.js
+++ server/db.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
-//mongoose.connect('mongodb://localhost:27017/fed-mongoose');
+const DB_URL = process.env.DB_URL || 'mongodb://localhost:27017/fed-mongoose';
+
+mongoose.connect(DB_URL);
 
 module.exports = mongoose;
